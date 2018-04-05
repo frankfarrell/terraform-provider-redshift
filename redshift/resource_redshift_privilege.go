669a577c8fc38bc75a59e1c98746b7c17e1108b4
frankfarrell <ofearghp@gmail.com>
2018-04-05 11:11:01 +0100

    chore: Placeholders for future resources

---
+++ redshift/resource_redshift_privilege.go
@@ -0,0 +1,3 @@
+package redshift
+
+
---
+++ redshift/resource_redshift_schema.go
@@ -0,0 +1,6 @@
+package redshift
+
+//Simple resource name and owner
+
+//https://docs.aws.amazon.com/redshift/latest/dg/r_CREATE_SCHEMA.html
+//CREATE SCHEMA [ IF NOT EXISTS ] schema_name [ AUTHORIZATION username ]
